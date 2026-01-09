bdb822acc4e9c24a869c52e31dfb83516bba026a
sudipta bhowmik <sudipta.bhowmik@ext.airbnb.com>
2026-01-09 21:39:57 +0530

    add dependency and add lombok

--- src/main/java/com/foodorder/dto/PlaceOrderDto.java
+++ src/main/java/com/foodorder/dto/PlaceOrderDto.java
@@ -1,11 +1,10 @@
 package com.foodorder.dto;
 
 import com.foodorder.enums.PaymentMethod;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class PlaceOrderDto {
--- src/main/java/com/foodorder/dto/UserRegistrationDto.java
+++ src/main/java/com/foodorder/dto/UserRegistrationDto.java
@@ -1,11 +1,10 @@
 package com.foodorder.dto;
 
 import com.foodorder.enums.UserRole;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserRegistrationDto {
